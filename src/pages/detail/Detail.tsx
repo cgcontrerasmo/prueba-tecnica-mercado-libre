328441c3ecc4fcbf46b0736d7e44f0f25a31101e
cgcontrerasm <cgcontrerasm@unal.edu.co>
2024-01-20 12:41:23 -0500

    ✨  Se  creó y se avanzo el la pantalla de descripción de un producto

---
+++ src/pages/detail/Detail.tsx
@@ -0,0 +1,14 @@
+import BreadCrumbs from "components/atoms/breadCrumbs/BreadCrumbs";
+import "./Detail.scss";
+import ResultDetail from "components/molecules/resultDetail/ResultDetail";
+
+const Detail = () => {
+  return (
+    <div>
+      <BreadCrumbs />
+      <ResultDetail />
+    </div>
+  );
+};
+
+export default Detail;
